f6900d20ea8291928d01b79221d6399c708fd6db
Andyrei <radoacaandrei3@gmail.com>
2024-10-06 18:50:53 +0200

    Add List

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { ModeToggle } from "@/components/ModeToggle";
 import type { Metadata } from "next";
 import "./globals.css";
+import HeaderComponent from "@/components/Header";
  
 export const metadata: Metadata = {
   title: "ShhpingList",
@@ -17,9 +18,14 @@ export default function RootLayout({
     <html lang="en" className=""  suppressHydrationWarning>
       <body className="">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-          </ThemeProvider>
-        </body>
+          <div className="flex min-h-screen w-full flex-col">
+            <HeaderComponent />
+            <main className="main">
+              {children}
+            </main>
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,44 @@
-import HeaderComponent from "@/components/Header";
-import { getSession } from "@/lib/auth/lib.user";
-import Image from "next/image";
+'use client'
 
-export default async function Home() {
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Dispatch, SetStateAction, useState } from "react";
 
-  const session = await getSession();
+type ListItem = {
+  id: string | number
+  title: string
+  checked: boolean
+  quantity: number
+}
+
+export default function Home() {
+  const [list, setList] = useState<ListItem[]>()
+  const [itemTitle, setItemTitle] = useState("")
+
+  function handleAddToList(){
+    let newItem: ListItem = {
+      id: Date.now(),
+      title: itemTitle,
+      checked: false,
+      quantity: 0,
+    }
+    setList([...list??[], newItem])
+  }
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
-    
-    <HeaderComponent />
+    <div className="">
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="text" onChange={e=>setItemTitle(e.target.value)} placeholder="What you want to add?" />
+          <Button type="button" onClick={handleAddToList}>Subscribe</Button>
+        </div>
+        {list?.map((item)=>{
+          return <>
+              <p>{item.title}</p>
+              <p>{item.quantity}</p>
+              <p>{item.checked}</p>
+          </>
 
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-  
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-
-      </div>
-    </main>
+        })}
     </div>
   );
 }
