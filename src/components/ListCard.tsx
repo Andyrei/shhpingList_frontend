c44deb012f25a58eb13497cf0d7de2af4b6a5f5d
Andyrei <radoacaandrei3@gmail.com>
2024-10-07 09:15:42 +0200

    Add: list card component

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import BinIcon from '../icons/binIcon';
 import CheckIcon from "@/icons/checkIcon";
 import { Button } from "@/components/ui/button";
@@ -10,18 +10,27 @@ type Props = {
     handleDel: Function
 }
 
+function ShapeShift({quantity, shift}: {quantity: number, shift: boolean}){
+    if(shift) return <input type="number" value={quantity} />
+    else return <p>{quantity}</p>
+}
 export default function ListCard({item, handleCheck, handleDel}: Props) {
+  
+    const [shift, setShift] = useState(false)
 
-  return (
-    <div key={item.id} className={`flex justify-between border ${item.checked ? "item_checked" : ''}`}>
-        <div>
-            <p>Title: {item.title}</p>
-            <div>Quantity: {item.quantity}</div>
+    function handleShapeShift(){
+        setShift(true)
+    }
+    return (
+        <div key={item.id} className={`flex justify-between border ${item.checked ? "item_checked" : ''}`}>
+            <div>
+                <p>Title: {item.title}</p>
+                <div onDoubleClick={handleShapeShift} >Quantity: <ShapeShift quantity={item.quantity} shift={shift}/> </div>
+            </div>
+            <div className="flex flex-col">
+                <Button type="button" onClick={()=>handleCheck(item.id)} > <CheckIcon /> </Button>
+                <Button type="button" onClick={()=>handleDel(item.id)} > <BinIcon /> </Button>
+            </div>
         </div>
-        <div className="flex flex-col">
-            <Button type="button" onClick={()=>handleCheck(item.id)}> <CheckIcon /> </Button>
-            <Button type="button" onClick={()=>handleDel(item.id)}> <BinIcon /> </Button>
-        </div>
-  </div>
-  )
+     )
 }
